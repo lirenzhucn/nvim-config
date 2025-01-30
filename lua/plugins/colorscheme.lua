1b92be6d44ecb270605dce66b28cdaded3d93c7e
Liren Zhu <liren.zhu.cn@gmail.com>
2025-01-30 14:02:16 -0800

    change colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,31 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    lazy = false,
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- init = function()
+    --   vim.cmd.colorscheme 'catppuccin-mocha'
+    -- end,
+  },
+  {
+    'slugbyte/lackluster.nvim',
+    lazy = false,
+    -- priority = 1000,
+    -- init = function()
+    --   -- vim.cmd.colorscheme 'lackluster'
+    --   -- vim.cmd.colorscheme 'lackluster-hack' -- my favorite
+    --   vim.cmd.colorscheme 'lackluster-mint'
+    -- end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
     init = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 }
