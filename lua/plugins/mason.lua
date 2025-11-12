824c8d74eee09cf2b9a8cb10712f2f53bbcbe7f2
Liren Zhu <liren.zhu.cn@gmail.com>
2025-11-12 06:25:32 -0800

    add roslyn lsp (csharp)

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+--- @module 'lazy'
+--- @type LazySpec
+return {
+  'williamboman/mason.nvim',
+  config = function()
+    require('mason').setup {
+      registries = {
+        'github:mason-org/mason-registry',
+        'github:Crashdummyy/mason-registry',
+      },
+    }
+  end,
+}
