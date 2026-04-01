b27aab3c6f97e6b9c91e017c199b08e6536d0a25
Liren Zhu <liren.zhu.cn@gmail.com>
2026-03-31 22:28:38 -0700

    remove duplicate config; upgrade telescope

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,18 +4,6 @@ return {
   { -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
   },
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
   { -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',
